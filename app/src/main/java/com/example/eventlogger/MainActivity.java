06967f25eff93d86275ed2d1822ee89e684d572b
Xzirez <futard@hotmail.no>
2019-05-21 15:31:04 +0200

    test

---
+++ app/src/main/java/com/example/eventlogger/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.eventlogger;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            int commit = getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new events_fragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_events:
+                            selectedFragment = new events_fragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new profile_fragment();
+                            break;
+                        case R.id.nav_chat:
+                            selectedFragment = new chat_fragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
